b71a1f580e99010d3e9485dc32a8eb5ea3bcad16
Christophe '116' Loiseau <116@lab0.net>
2019-07-19 23:05:37 +0200

    Partial fizzbuzz

---
+++ urbi_orbi.pl
@@ -0,0 +1,31 @@
+
+use Lingua::Romana::Perligata;
+
+adnota Periclitor
+
+adnota Quantitas tum maximum inquementum tum biguttam tum lacunam egresso scribe.
+adnota da meo maximo vestibulo perlegementum.
+novumversum egresso scribe.
+
+meo maximo C da.
+meis listis conscribementa I tum maximum da.
+dum damentum nexto listis decapitamentum fac
+    sic
+
+    meo valo nextum tum III recidementum da.
+    meo comodo nextum tum V recidementum da.
+
+    nextum comementum tum lacunam egresso scribe.
+    meo phraso lacuna da.
+    si nullum tum valum aequalitam fac
+        sic
+            dictum sic Urbi cis inquementum egresso scribe.
+        cis
+    si nullum tum comodum aequalitam fac
+        sic
+            dictum sic Orbi cis inquementum egresso scribe.
+        cis
+    novumversum egresso scribe.
+    
+cis
+novumversum egresso scribe.
